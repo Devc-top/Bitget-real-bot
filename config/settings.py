341dbfbc0ab1cf0103c79f3d1b5b0d3fd727c7f2
TopPresident <top.devc@gmail.com>
2025-12-31 16:40:40 +0100

    Create settings.py

---
+++ config/settings.py
@@ -0,0 +1,10 @@
+import os
+
+BITGET_API_KEY = os.getenv("BITGET_API_KEY")
+BITGET_SECRET = os.getenv("BITGET_SECRET")
+BITGET_PASSPHRASE = os.getenv("BITGET_PASSPHRASE")
+
+BASE_URL = "https://api.bitget.com"
+
+SYMBOL = "BTCUSDT"
+TRADE_AMOUNT_USDT = 10   # client/investor amount
